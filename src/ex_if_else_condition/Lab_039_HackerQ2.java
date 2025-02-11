ddc3034bcdcfde66f407049e9b26efbd898be197
jamil921112 <arshad241297@gmail.com>
2025-02-11 17:10:01 +0530

    dd

--- src/ex_if_else_condition/Lab_039_HackerQ2.java
+++ src/ex_if_else_condition/Lab_039_HackerQ2.java
@@ -12,37 +12,39 @@ public class Lab_039_HackerQ2 {
         // or scalene (no sides are equal). Use an if-else statement to classify the triangle.
 // side 1, side 2, side3 ->eq , iso ,scalene
 
-    //   logic building
+        //   logic building
         //   step 1 - find inp/output
         // input | side 1, side 2, side3 -> data types-> double
         // output | SOP -> string message -> equilateral, isosceles, scalene
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a the side 1:  ");
-        double side1  = input.nextDouble();
 
-        System.out.println("Enter a the side 2:  ");
-        double side2  = input.nextDouble();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a the side 1:  ");
+            double side1 = input.nextDouble();
+
+            System.out.println("Enter a the side 2:  ");
+            double side2 = input.nextDouble();
 
 
-        System.out.println("Enter a the side 3:  ");
-        double side3  = input.nextDouble();
+            System.out.println("Enter a the side 3:  ");
+            double side3 = input.nextDouble();
 
-       //2. logic /rough logic
-        // if side1==side2 and side2==side3 , side1==side3 -> equilateral triangle
-        // else if side1==side2 || side2==side3|| side3== side1 -> isosceles
-        // else > scalene
+            //2. logic /rough logic
+            // if side1==side2 and side2==side3 , side1==side3 -> equilateral triangle
+            // else if side1==side2 || side2==side3|| side3== side1 -> isosceles
+            // else > scalene
 
-        if(side1==side2&& side2==side3 && side1==side3){
-            System.out.println("The triangle is equilateral");
-        } else if ( side1==side2 || side2==side3|| side3== side1) {
-            System.out.println("The triangle is isosceles");
-        }else {
-            System.out.println("The triangle is  scalene");
+            if (side1 == side2 && side2 == side3 && side1 == side3) {
+                System.out.println("The triangle is equilateral");
+            } else if (side1 == side2 || side2 == side3 || side3 == side1) {
+                System.out.println("The triangle is isosceles");
+            } else {
+                System.out.println("The triangle is  scalene");
+
+
+            }
 
 
         }
 
-
     }
-}
